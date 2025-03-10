7735a2371df38a2db896670f7c40190c97c32601
stephan <guroo@gmx.de>
2025-03-10 20:47:09 +0100

    notes

--- attachments/03 React Essentials/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,14 +1,16 @@
 import MyComponent from "./MyComponent.jsx";
 import TabButton from "./TabButton.jsx";
 import { useState } from 'react';
-
+import {} from './assets/data.js';
 
 function App() {
   //must be called at the top level of a Component function (or custom hooks)
-  useState();
+  //changes in state will trigger re-execution of component
+  const [numberOfItems, setNumberOfItems] = useState(1);
+  
   var count = 0;
   let handleClick = () => {console.log("clicked:" + count++);}
-
+  var buttons = Array(numberOfItems).keys().map(() => <TabButton clickHandler={handleClick} />);
   return (
     <div>
       <header>
@@ -19,13 +21,12 @@ function App() {
       <main>
         <h3>Time!</h3>
         <p>
+          <button onClick={() => setNumberOfItems(numberOfItems+1)}>Add line</button>
           <MyComponent text="my props">
             <ol>
-              <TabButton clickHandler={handleClick} />
-              
+              {buttons}
             </ol>
-          </MyComponent>
-           
+          </MyComponent>           
         </p>
       </main>
     </div>
