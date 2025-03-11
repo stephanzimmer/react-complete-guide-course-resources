ba56ecfe42345a83985a66dcf62fb406b61a4d90
stephan <guroo@gmx.de>
2025-03-11 18:13:06 +0100

    activePlayer

--- attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,11 +2,29 @@ import { useState } from 'react';
 import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx'
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 function App() {
   const [activePlayer, setActivePlayer] = useState('X')
+  const [gameTurns, setGameTurns] = useState([])
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-  function handleSelectSquare() {
+  function handleSelectSquare(row, col) {    
+    setGameBoard((prev) => {
+      let gb = [...gameBoard.map(inner => [...inner])]
+      gb[row][col] = activePlayer
+      return gb
+    })
+
     setActivePlayer(old => old == 'X' ? 'O' : 'X');
+    setGameTurns(old => {
+      const n = [{player: activePlayer, square: {row:row, col:col}}, ...old]
+      return n
+    })
   }
 
   return (
@@ -17,7 +35,7 @@ function App() {
           <Player name="Player 2" symbol="O" isActive={activePlayer == 'O'}/>
         </ol>
       
-        <GameBoard onSelectSquare={handleSelectSquare} playerSymbol={activePlayer}/>
+        <GameBoard gameBoard={gameBoard} onSelectSquare={handleSelectSquare} />
       </div>
     </main>
   )
--- attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,26 +1,5 @@
-import { useState } from 'react';
-
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-const initial = [null, null, null];
-
-export default function GameBoard({onSelectSquare, playerSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-    function handleSelect(row, col) {
-        console.log(row + " " + col)
-        setGameBoard((prev) => {
-            let gb = [...gameBoard.map(inner => [...inner])]
-            gb[row][col] = playerSymbol
-            return gb
-        })
-
-        onSelectSquare();
-    }
+export default function GameBoard({gameBoard, onSelectSquare}) {
+    
 
     return (
     <ol id="game-board">
@@ -30,7 +9,7 @@ export default function GameBoard({onSelectSquare, playerSymbol}) {
                 {row.map((cell, cellIndex) => 
                 <li key={rowIndex*10+cellIndex}>
                     
-                    <button onClick={() => handleSelect(rowIndex, cellIndex)}>{gameBoard[rowIndex][cellIndex]}</button>
+                    <button onClick={() => onSelectSquare(rowIndex, cellIndex)}>{gameBoard[rowIndex][cellIndex]}</button>
                 </li>)}
             </ol>    
         </li>)}
